e664cc6ad5551f01896afe239da38a501abb53b0
lukaleht <luka@verkkovirta.fi>
2026-02-10 13:11:52 +0200

    Delete api/hello directory

--- api/hello/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, req) {
-  context.log('Hello function processed a request.');
-
-  context.res = {
-    status: 200,
-    body: "Hello from Azure Static Web Apps!"
-  };
-};
-
