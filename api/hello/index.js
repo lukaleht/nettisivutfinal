0440887090dac1d8d4ab5405b665ded8ddb6a070
lukaleht <luka@verkkovirta.fi>
2026-02-10 13:14:24 +0200

    Update index.js

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,1 +1,9 @@
+module.exports = async function (context, req) {
+  context.log('Hello function processed a request.');
+
+  context.res = {
+    status: 200,
+    body: "Hello from Azure Static Web Apps!"
+  };
+};
 
